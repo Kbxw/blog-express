10d4b0b62e3e53bb4ff388bdbc69dea67df175ad
David <rocketbillyred@gmail.com>
2020-11-23 13:25:59 +0100

    beta 3.0

---
+++ app.js
@@ -0,0 +1,51 @@
+/* { ----------- EXPRESS ----------- } */
+const express = require('express');
+const app = express();
+
+/* { ----------- MODELS & ROUTERS ----------- } */
+require("./db/mongoose");
+const Blog = require('./models/blog');
+const blogRouter = require('./routers/blog');
+
+/* { ----------- PORT ----------- } */
+const port = process.env.PORT || 3000
+
+/* { ----------- APP LISTEN ----------- } */
+app.listen(port, ()=>{
+    console.log(`Listening to port ${port}`);
+});
+
+/* { ----------- EJS VIEW ENGINE ----------- } */
+app.set('view engine', 'ejs');
+
+/* { ----------- MIDDLEWARE ----------- } */
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log('new request made:');
+    console.log('host: ', req.hostname);
+    console.log('path: ', req.path);
+    console.log('method: ', req.method);
+    next();
+});
+
+/* { ----------- RUTAS WEB ----------- } */
+app.get('/', async (req, res) => {
+    await Blog.find({}).then((data) => {
+        res.render('index', { blog: data })
+    });
+});
+app.get('/crear', (req, res) => {
+    res.render('crear', {  })
+});
+
+/* { ----------- API ROUTING ----------- } */
+// EXAMPLE: app.use('/api', modelRouter)
+app.use('/api', blogRouter);
+
+/* { ----------- EXPRESS ----------- } */
+app.use((req, res) => {
+    res.status(404).render('404', {});
+});
---
+++ models/blog.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const Blog = mongoose.model('Blog', {
+    title: {
+        type: String,
+        required: true
+    },
+    snippet: {
+        type: String,
+        required: true,
+    }
+})
+
+
+module.exports = Blog
